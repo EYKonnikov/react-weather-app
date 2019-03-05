4ea38627f3fb9e46d603670511e52b5bb78acfeb
Evgeny Konnikov-Lehmann <ykonnikov@yandex.com>
2019-03-05 15:26:01 +0100

    Add files via upload

---
+++ precache-manifest.7694259ebbda5cc63803089ea4843059.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae3bf4adb3e6d82d6fea015c91f1cfa8",
+    "url": "/react-weather-app/static/media/polygon.ae3bf4ad.jpg"
+  },
+  {
+    "revision": "72a6b7ef5541b624727a",
+    "url": "/react-weather-app/static/js/runtime~main.72a6b7ef.js"
+  },
+  {
+    "revision": "569a412d2703a0462b6b",
+    "url": "/react-weather-app/static/js/main.569a412d.chunk.js"
+  },
+  {
+    "revision": "dada628caae169205df6",
+    "url": "/react-weather-app/static/js/1.dada628c.chunk.js"
+  },
+  {
+    "revision": "569a412d2703a0462b6b",
+    "url": "/react-weather-app/static/css/main.2022f2d4.chunk.css"
+  },
+  {
+    "revision": "dada628caae169205df6",
+    "url": "/react-weather-app/static/css/1.176851e3.chunk.css"
+  },
+  {
+    "revision": "501fe3a7fa76cc3eb1d6775c0ce8f809",
+    "url": "/react-weather-app/index.html"
+  }
+];
